77f7f2fc577bd6137bc4284f07e9f706d6deeac4
MrMerin <2938889184@qq.com>
2023-03-10 00:11:18 +0800

    增加首页和登录页面组件，并配置路由

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
+import { MainLayoutComponent } from './layout/main-layout/main-layout.component';
+import { BaseLayoutComponent } from './layout/base-layout/base-layout.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: MainLayoutComponent,
+  },
+  {
+    path: 'login', component: BaseLayoutComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/layout/base-layout/base-layout.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'base-layout',
+  templateUrl:'./base-layout.component.html',
+  styleUrls: ['./base-layout.component.sass']
+})
+
+export class BaseLayoutComponent {
+
+}
