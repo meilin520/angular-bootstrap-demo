6341ffd2c77ab1e2cf741b576d31e75bf974f37d
MrMerin <2938889184@qq.com>
2023-03-09 18:27:31 +0800

    删除无用组件

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TopBarComponent } from './top-bar/top-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TopBarComponent,
   ],
   imports: [
     BrowserModule,
